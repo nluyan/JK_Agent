52173362925ec60a47407ed42ce6bd3ea4d2217d
Luyan <luy@live.com>
2025-11-28 11:06:04 +0800

    优化编译脚本

--- GoClient/build.ps1
+++ GoClient/build.ps1
@@ -1,11 +1,13 @@
+$env:CGO_ENABLED = "0"
+
 $env:GOOS = "windows"
 $env:GOARCH = "amd64"
-go build -o output/AgentClient64.exe
+go build -o output/win64/AgentClient.exe
 
 $env:GOOS = "windows"
 $env:GOARCH = "386"
-go build -o output/AgentClient32.exe
+go build -o output/win32/AgentClient.exe
 
 $env:GOOS = "linux"
 $env:GOARCH = "amd64"
-go build -o output/AgentClient
+go build -o output/linux/AgentClient
