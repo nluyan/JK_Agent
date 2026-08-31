d7e54aa6d00014b3614e09f98a42987a9fda1e22
Luyan <luy@live.com>
2026-08-31 15:42:44 +0800

    发布支持打印机驱动的2.19版本

--- AgentClient/Settings.cs
+++ AgentClient/Settings.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +6,5 @@ using System.Threading.Tasks;
 
 internal class Settings
 {
-	public const string Version = "2.11";
+	public const string Version = "2.19";
 }
